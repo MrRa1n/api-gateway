1eaebbff0960dffff837880f937d5258775223ba
Toby Cook <toby.cook@ba.com>
2024-07-19 15:56:43 +0100

    adds discovery server for services

--- inventory-service/src/main/java/com/leighton/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/leighton/inventoryservice/controller/InventoryController.java
@@ -1,9 +1,12 @@
 package com.leighton.inventoryservice.controller;
 
+import com.leighton.inventoryservice.dto.InventoryResponse;
 import com.leighton.inventoryservice.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/inventory")
@@ -12,9 +15,9 @@ public class InventoryController {
 
     private final InventoryService inventoryService;
 
-    @GetMapping("/{skuCode}")
+    @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public boolean isInStock(@PathVariable("sku-code") String skuCode) {
+    public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode) {
         return inventoryService.isInStock(skuCode);
     }
 }
--- order-service/src/main/java/com/leighton/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/leighton/orderservice/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.leighton.orderservice.service;
 
+import com.leighton.orderservice.dto.InventoryResponse;
 import com.leighton.orderservice.dto.OrderLineItemsDto;
 import com.leighton.orderservice.dto.OrderRequest;
 import com.leighton.orderservice.model.Order;
@@ -8,7 +9,9 @@ import com.leighton.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -19,6 +22,8 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
 
+    private final WebClient.Builder webClientBuilder;
+
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
@@ -26,6 +31,25 @@ public class OrderService {
                 .stream()
                 .map(this::mapToDto).toList();
         order.setOrderLineItemsList(orderLineItems);
+
+        List<String> skuCodes = order.getOrderLineItemsList().stream()
+                .map(OrderLineItems::getSkuCode)
+                .toList();
+
+        // Call Inventory Service and place order if in stock
+        InventoryResponse[] inventoryResponses = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                .retrieve()
+                .bodyToMono(InventoryResponse[].class)
+                .block();
+
+        boolean allProductsInStock = Arrays.stream(inventoryResponses)
+                .allMatch(InventoryResponse::isInStock);
+
+        if (!allProductsInStock) {
+            throw new IllegalArgumentException("Product is not in stock, please try again.");
+        }
 
         orderRepository.save(order);
     }
